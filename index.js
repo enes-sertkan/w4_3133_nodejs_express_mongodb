48220abe1310110af535334cf2fa0c5244881a39
enes-sertkan <enessertkan451@gmail.com>
2024-02-02 19:28:45 -0500

    Commiting initial setup

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json()); // Make sure it comes back as json
 
 //TODO - Replace you Connection String here
-mongoose.connect('mongodb+srv://sa:rrfYrY3mSzHSgzJR@cluster0.qa3t4.mongodb.net/db_f2021_comp3123?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://enessertkan:Password123@cluster0.55mfjdg.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(success => {
@@ -16,5 +16,6 @@ mongoose.connect('mongodb+srv://sa:rrfYrY3mSzHSgzJR@cluster0.qa3t4.mongodb.net/d
 });
 
 app.use(employeeRouter);
+
 
 app.listen(8081, () => { console.log('Server is running...') });
